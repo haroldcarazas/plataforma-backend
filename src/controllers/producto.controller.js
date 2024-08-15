2ab581c1963dba2de8ebdffbbe0742127fce15c8
harold2001 <hcarazasnires@gmail.com>
2024-08-15 12:48:40 -0500

    segunda ruta por ID, y validación

--- src/controllers/producto.controller.js
+++ src/controllers/producto.controller.js
@@ -5,6 +5,16 @@ class ProductoController {
     const productos = await Producto.find()
     res.json(productos)
   }
+
+  static async find (req, res) {
+    try {
+      const { id } = req.params
+      const producto = await Producto.findById(id)
+      res.json(producto)
+    } catch (error) {
+      res.status(500).json({ message: error.message })
+    }
+  }
 }
 
 export default ProductoController
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import ProductoController from '../controllers/producto.controller.js'
+import { validateId } from '../middlewares/middleware.js'
 
 const router = Router()
 
 router.get('/', ProductoController.index)
+router.get('/:id', validateId, ProductoController.find)
 
 export default router
