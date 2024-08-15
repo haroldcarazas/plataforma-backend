0d728b85b925725389211dd13f762e02950a250f
harold2001 <hcarazasnires@gmail.com>
2024-08-15 11:49:52 -0500

    primer commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import { connectDB } from './config/db.js'
+import { PORT } from './config/config.js'
+import productosRoutes from './routes/productos.routes.js'
+
+const app = express()
+
+app.use('api/productos', productosRoutes)
+
+connectDB()
+  .then(() => app.listen(PORT, () => console.log(`http://localhost:${PORT}`)))
+  .catch(error => console.log(error))
