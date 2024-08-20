6441f9917e3ef3aeae80be3b1ab42c3c6c8399f3
harold2001 <hcarazasnires@gmail.com>
2024-08-20 11:41:50 -0500

    auth routes y controller

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+class AuthController {
+  static async register () {
+
+  }
+
+  static async login () {
+
+  }
+
+  static async me () {
+
+  }
+}
+
+export default AuthController
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import express from 'express'
 import { connectDB } from './config/db.js'
 import { PORT } from './config/config.js'
-import productosRoutes from './routes/productos.routes.js'
-import ordenesRoutes from './routes/ordenes.routes.js'
+import authRoutes from './routes/auth.routes.js'
+// import productosRoutes from './routes/productos.routes.js'
+// import ordenesRoutes from './routes/ordenes.routes.js'
 
 const app = express()
 
 app.use(express.json())
-app.use('/api/productos', productosRoutes)
-app.use('/api/ordenes', ordenesRoutes)
+
+app.use('/api/auth', authRoutes)
+// app.use('/api/productos', productosRoutes)
+// app.use('/api/ordenes', ordenesRoutes)
 
 connectDB()
   .then(() => app.listen(PORT, () => console.log(`http://localhost:${PORT}`)))
