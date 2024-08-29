c4c793258cddcdf378baa8fc8f516f01ee85acde
harold2001 <hcarazasnires@gmail.com>
2024-08-29 12:53:57 -0500

    origin modificado

--- src/config/config.js
+++ src/config/config.js
@@ -5,4 +5,4 @@ config()
 export const PORT = process.env.PORT || 3000
 export const SECRET_KEY = process.env.SECRET_KEY
 export const DB_URL = process.env.DB_URL
-export const ALLOWED_ORIGINS = ['http://localhost:5173', 'https://haroldcarazas.github.io/']
+export const ALLOWED_ORIGINS = ['http://localhost:5173', 'https://haroldcarazas.github.io']
